6c00b83db594d52ae58f526f9ae59190622bd9a2
EC2 Default User <ec2-user@ip-172-31-43-12.us-east-2.compute.internal>
2018-06-14 07:27:30 +0000

    post

--- microposts/routes/web.php
+++ microposts/routes/web.php
@@ -10,9 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'MicropostsController@index');
 
 Route::get('/', function () {
-    return view('welcome');
+return view('welcome');
 });
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -23,5 +24,6 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
