eccb32f99679ea48b144c42952c1d82773ac594d
EC2 Default User <ec2-user@ip-172-31-43-12.us-east-2.compute.internal>
2018-06-19 03:45:52 +0000

    follow, unfollow

--- microposts/routes/web.php
+++ microposts/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'MicropostsController@index');
+
 
 Route::get('/', function () {
 return view('welcome');
@@ -23,7 +23,17 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::get('/', 'MicropostsController@index');
+
 Route::group(['middleware' => ['auth']], function () {
 Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+
+Route::group(['prefix' => 'users/{id}'], function () {
+Route::post('follow', 'UserFollowController@store')->name('user.follow');
+Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+Route::get('followings', 'UsersController@followings')->name('users.followings');
+Route::get('followers', 'UsersController@followers')->name('users.followers');
+});
+
 Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
