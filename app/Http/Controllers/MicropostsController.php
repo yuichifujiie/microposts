ce010325b521847ab9fcb511c3c19d8c1d80d1f0
EC2 Default User <ec2-user@ip-172-31-43-12.us-east-2.compute.internal>
2018-06-22 03:46:38 +0000

    delete d

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -13,7 +13,7 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            dd($user);
+
             $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
